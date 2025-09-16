20b4ef8e312234861eb19a5712c612998da15fe1
hemanthganeshvilluri <hemanthganeshvilluri@gmail.com>
2025-09-16 22:13:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def predict():
         # Make prediction
         prediction = model.predict(input_data)[0]
 
-        return jsonify(f'Predicted Insurance Charges: ${prediction:,.2f}')
+        return jsonify(f'Prediction: ${prediction:,.2f}')
 
     except Exception as e:
         return jsonify(f'Error: {str(e)}')
@@ -45,6 +45,7 @@ def predict():
 if __name__ == "__main__":
     # Run app on 0.0.0.0 so it's accessible externally
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
